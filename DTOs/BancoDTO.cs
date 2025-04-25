afa7c943a263a14b231f6851dc6e35beb18fe07f
MartinGBB <martingbrazon@gmail.com>
2025-04-25 00:11:45 -0300

    feat: criação DTOs

---
+++ DTOs/BancoDTO.cs
@@ -0,0 +1,7 @@
+public class BancoDto
+{
+    public int Id { get; set; }
+    public string Nome { get; set; }
+    public string Codigo { get; set; }
+    public decimal PercentualJuros { get; set; }
+}
---
+++ DTOs/BoletoDTO.cs
@@ -0,0 +1,12 @@
+public class BoletoDto
+{
+    public int Id { get; set; }
+    public string NomePagador { get; set; }
+    public string DocumentoPagador { get; set; }
+    public string NomeBeneficiario { get; set; }
+    public string DocumentoBeneficiario { get; set; }
+    public decimal Valor { get; set; }
+    public DateTime DataVencimento { get; set; }
+    public string? Observacao { get; set; }
+    public int BancoId { get; set; }
+}
